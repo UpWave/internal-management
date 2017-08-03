3b092bb45eba78bb199d4d962a941242f980c6cf
Markiyan Ivancho <markiyan.ivancho@upwave.net>
2017-08-03 11:59:19 -0400

    fix tabs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
     data = access_token.info
     user = User.where(email: data['email']).first
 
-     unless user
-         user = User.create(
+    unless user
+      user = User.create(
             email: data['email'],
             password: Devise.friendly_token[0,20]
-         )
-     end
+            )
+    end
     user
   end
 end
