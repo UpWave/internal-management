283aa0ee293ac355421e33a8cbb5233edf4c7a0a
Markiyan Ivancho <markiyan.ivancho@upwave.net>
2017-08-03 16:19:53 -0400

    basic crud

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "User updated"
+      redirect_to users_path
+    else
+      flash[:error] = "Something went wrong"
+      redirect_to users_path
+    end
+  end
+
+
+  def user_params
+      params.require(:user).permit(:email, :role)
+    end
+
+end
