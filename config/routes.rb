17a23852b4ff6e1d30459f4f4efe0d410c84a832
Markiyan Ivancho <markiyan.ivancho@upwave.net>
2017-08-04 16:10:53 -0400

    minor update

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    resource :registration,
-    only: [:new, :create, :edit, :update],
-    path: 'users',
-    path_names: { new: 'sign_up' },
-    controller: 'devise/registrations',
-    as: :user_registration do
-      get :cancel
-      end
-    end
   resources :users
   get 'pages/index'
   root to: "pages#index"
