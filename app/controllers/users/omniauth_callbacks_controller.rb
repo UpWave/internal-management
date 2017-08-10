6d2ac3d14076e06b2f7d1fed1ce90b84781c653e
Markiyan Ivancho <markiyan.ivancho@upwave.net>
2017-08-10 18:06:30 -0400

    one more code refactoring

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth = request.env["omniauth.auth"]
     if current_user
       unless current_user.has_google?
-        current_user.connect_google(auth)
+        Identity.create(:uid => auth.uid, :provider => auth.provider, :user_id => current_user.id)
         redirect_to root_path, notice: "Successfully linked Google account!"
       else
         redirect_to root_path, notice: "Google account already linked!"
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
         sign_in_and_redirect @user, event: :authentication
         unless current_user.has_google?
-          current_user.connect_google(auth)
+          Identity.create(:uid => auth.uid, :provider => auth.provider, :user_id => current_user.id)
         end
       else
         session['devise.google_data'] = auth.except(:extra) 
@@ -27,7 +27,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth = request.env["omniauth.auth"]
     if current_user
       unless current_user.has_trello?
-        current_user.connect_trello(auth)
+        Identity.create(:uid => auth.uid, :provider => auth.provider, :user_id => current_user.id, :access_token => auth.extra.access_token.token, :secret_token => auth.extra.access_token.secret)
         redirect_to root_path, notice: "Successfully linked Trello account!"
       else
         redirect_to root_path, notice: "Trello account already linked!"
@@ -38,9 +38,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Trello'
         sign_in_and_redirect @user, event: :authentication
         if current_user.has_trello?
-          current_user.update_trello(auth)    
+          Identity.find_by(:uid => auth.uid, :provider => auth.provider, :user_id => current_user.id).update_attributes(:access_token => auth.extra.access_token.token, :secret_token => auth.extra.access_token.secret)    
         else 
-          current_user.connect_trello(auth)
+          Identity.create(:uid => auth.uid, :provider => auth.provider, :user_id => current_user.id, :access_token => auth.extra.access_token.token, :secret_token => auth.extra.access_token.secret)
         end
       else
         session['devise.trello_data'] = auth 
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,18 +19,6 @@ class User < ApplicationRecord
     identities.pluck("provider").count == 2
   end
 
-  def connect_google(auth)
-    Identity.create(:uid => auth.uid, :provider => auth.provider, :user_id => id)  
-  end
-
-  def connect_trello(auth)
-    Identity.create(:uid => auth.uid, :provider => auth.provider, :user_id => id, :access_token => auth.extra.access_token.token, :secret_token => auth.extra.access_token.secret)  
-  end
-
-  def update_trello(auth)
-    Identity.find_by(:uid => auth.uid, :provider => auth.provider, :user_id => id).update_attributes(:access_token => auth.extra.access_token.token, :secret_token => auth.extra.access_token.secret)
-  end
-
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_create do |user|  
       user.password = Devise.friendly_token[0,20]
