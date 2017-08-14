dc56fc398daeed8e2cf0fe7992d10b37e85491ab
Mark <markiyan.ivancho@upwave.net>
2017-08-14 11:46:56 -0400

    range filtering

--- app/controllers/timelogs_controller.rb
+++ app/controllers/timelogs_controller.rb
@@ -9,10 +9,6 @@ class TimelogsController < ApplicationController
     unless params[:filter]
       @timelogs = current_user.timelogs
     else
-      if params[:start_date]
-        @timelogs = current_user.timelogs.where("start_time >= :start_date AND end_time <= :end_date",
-    {start_date: params[:start_date], end_date: params[:end_date]})
-      end
       if params[:filter] == "ends_week"
         @timelogs = current_user.timelogs.where(end_time: (Date.today..(Date.today + 7.days)))
       elsif params[:filter] == "ends_month"
@@ -21,7 +17,13 @@ class TimelogsController < ApplicationController
         @timelogs = current_user.timelogs.sort_by &params[:filter].to_sym
       end
     end
+    unless (params[:start_date].empty? || params[:end_date].empty?) 
+      @timelogs = current_user.timelogs.where("start_time >= :start_date AND end_time <= :end_date",
+      {start_date: params[:start_date], end_date: params[:end_date]})
+    else
+      flash[:error] = "Invalid date input"
     end
+  end
 
   def create
     @timelog = current_user.timelogs.build(timelogs_params)
