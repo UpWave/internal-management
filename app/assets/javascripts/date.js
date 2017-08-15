fa4c22e8aad30cfd37f6efe36121d48641b01c07
Mark <markiyan.ivancho@upwave.net>
2017-08-15 14:03:50 -0400

    js update

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $('input[type="date"]').change(function() {
     if ( $('#start_date').val() != "" && $('#end_date').val() != "") {
       $('#applyDateButton').css("visibility", "visible");
-    } 
-  });
+    } else {
+      $('#applyDateButton').css("visibility", "hidden");
+    }
+  }); 
 });
