24eb3e22e21c464138389bd6926521955e6c69c3
Mark <markiyan.ivancho@upwave.net>
2017-09-06 16:28:36 -0400

    rebuilded user vacations

---
+++ app/javascript/packs/user_vacations/new_vacation.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NewVacation extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      startDate: 0,
+      endDate: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleStartDateChange = this.handleStartDateChange.bind(this);
+    this.handleEndDateChange = this.handleEndDateChange.bind(this);
+    this.checkSubmitVisibility = this.checkSubmitVisibility.bind(this);
+  }
+
+  handleClick() {
+    const startDate = this.state.startDate;
+    const endDate = this.state.endDate;
+    $.ajax({
+      url: '/api/v1/vacations',
+      type: 'POST',
+      beforeSend(xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')); },
+      data: { vacation: { start_date: startDate, end_date: endDate } },
+      success: () => {
+        this.props.handleSubmit();
+      },
+    });
+  }
+
+  handleStartDateChange(event) {
+    this.setState({ startDate: event.target.value }, () => {
+      this.checkSubmitVisibility();
+    });
+  }
+
+  handleEndDateChange(event) {
+    this.setState({ endDate: event.target.value }, () => {
+      this.checkSubmitVisibility();
+    });
+  }
+
+  checkSubmitVisibility() {
+    if (this.state.endDate > this.state.startDate) {
+      $('#submit').css('visibility', 'visible');
+    } else {
+      $('#submit').css('visibility', 'hidden');
+    }
+  }
+
+  render() {
+    return (
+      <div id="new_vacation">
+        <h3>Request a new vacation!</h3>
+        Start Date:
+        <input type="date" onChange={this.handleStartDateChange} /><br />
+        End Date:
+        <input type="date" onChange={this.handleEndDateChange} /><br />
+        <br />
+        <button id="submit" style={{ visibility: 'hidden' }} onClick={this.handleClick}>Submit</button><br />
+      </div>
+    );
+  }
+}
+
+NewVacation.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default NewVacation;
