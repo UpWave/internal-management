d129384cd2b32478320b806e74463811da2c9f07
Mark <markiyan.ivancho@upwave.net>
2017-09-15 16:57:39 -0400

    controller and spec update

--- app/controllers/api/v1/admin/user/skills_controller.rb
+++ app/controllers/api/v1/admin/user/skills_controller.rb
@@ -4,15 +4,9 @@ class Api::V1::Admin::User::SkillsController < Api::V1::BaseController
   def create
     @user_skill = UserSkill.new(user_skill_params)
     authorize @user_skill
+    @user = User.find(params[:user_skill][:user_id])
     if @user_skill.save
-      # Dunno what to do here
-      #
-      # It creates new record but then responds with
-      # ActionController::UrlGenerationError
-      # (No route matches {:action=>"show", :controller=>"api/v1/admin/user/skills",
-      # :user_id=>#<UserSkill id: 149, user_id: 36, skill_id: 52, rate: 6...
-      # missing required keys: [:id])
-      # respond_with :api, :v1, :admin, :user, @user_skill
+      respond_with :api, :v1, :admin, @user, @user_skill
     else
       render json: { errors: @user_skill.errors.full_messages }, status: 422
     end
