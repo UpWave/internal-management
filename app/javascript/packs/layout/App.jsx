8fc6660c559cae0eb299070b44bdd5aaf9894e33
Mark <markiyan.ivancho@upwave.net>
2017-09-21 11:38:10 -0400

    moved routes to another component; few fixes

--- app/javascript/packs/layout/App.jsx
+++ app/javascript/packs/layout/App.jsx
@@ -1,19 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './header';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import Header from './Header';
+import Routes from './Routes';
 
 class App extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      admin: false,
+      logged: false,
+      hasGoogle: false,
+      hasTrello: false,
+    };
+    this.signOutClick = this.signOutClick.bind(this);
+  }
+
+  componentWillMount() {
+    $.ajax({
+      url: '/auth/is_signed_in',
+      type: 'GET',
+      success: (data) => {
+        this.setState({ logged: data.signed_in });
+        if (data.signed_in) {
+          this.setState({ admin: data.user.role === 'admin' });
+          this.checkIdentities();
+        } else {
+          this.setState({ admin: false });
+        }
+      },
+    });
+  }
+
+  checkIdentities() {
+    $.ajax({
+      url: '/auth/check_identities',
+      type: 'GET',
+      success: (data) => {
+        this.setState({ hasGoogle: data.has_google });
+        this.setState({ hasTrello: data.has_trello });
+      },
+    });
+  }
+
+  signOutClick() {
+    $.ajax({
+      url: '/users/sign_out',
+      beforeSend(xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')); },
+      type: 'DELETE',
+      success: () => {
+        this.setState({ logged: false });
+        this.setState({ hasGoogle: false });
+        this.setState({ hasTrello: false });
+      },
+    });
+  }
+
   render() {
     return (
-      <div>
-        <Header />
-      </div>
+      <Router>
+        <div>
+          <Header
+            logged={this.state.logged}
+            admin={this.state.admin}
+            hasGoogle={this.state.hasGoogle}
+            hasTrello={this.state.hasTrello}
+            signOutClick={this.signOutClick}
+          />
+          <Routes
+            logged={this.state.logged}
+            admin={this.state.admin}
+          />
+        </div>
+      </Router>
     );
   }
 }
 
 ReactDOM.render(
-  <App />, document.getElementById('root'),
+  <div>
+    <App />
+  </div>,
+  document.getElementById('root'),
 );
 
 export default App;
