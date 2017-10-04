e1e31697b1c08175096d9cb7351d7b8dacc1b2c3
Roman Kan <roman95roman@gmail.com>
2017-10-04 15:02:08 +0300

    move from serializer to model

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -9,39 +9,4 @@ class UserSerializer < ActiveModel::Serializer
   def photo
     self.object.photo
   end
-
-  def sick_leave_approved
-    vacations("approved", "sick leave")
-  end
-
-  def sick_leave_pending
-    vacations("pending", "sick leave")
-  end
-
-  def day_offs_approved
-    vacations("approved", "unpaid day offs")
-  end
-
-  def day_offs_pending
-    vacations("pending", "unpaid day offs")
-  end
-
-  def planned_vac_approved
-    vacations("approved", "planned vacation")
-  end
-
-  def planned_vac_pending
-    vacations("pending", "planned vacation")
-  end
-
-
-  def vacations(status, type)
-    vacations = self.object.vacations.where(["status = ? and type = ?", Vacation.statuses["#{status}"],  Vacation.types["#{type}"]])
-    days = 0
-    vacations.each do |v|
-      days += (v.end_date - v.start_date).to_i
-    end
-    days
-  end
-
 end
