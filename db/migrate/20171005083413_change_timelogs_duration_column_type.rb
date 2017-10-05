e691cebc3501c01f6d3c65d3524fdc256b072064
Roman Kan <roman95roman@gmail.com>
2017-10-05 12:27:01 +0300

    fix migration

--- db/migrate/20171005083413_change_timelogs_duration_column_type.rb
+++ db/migrate/20171005083413_change_timelogs_duration_column_type.rb
@@ -1,7 +1,7 @@
 class ChangeTimelogsDurationColumnType < ActiveRecord::Migration[5.1]
   def up
-    # remove_column :timelogs, :duration
-    # add_column :timelogs, :duration, :string
+    remove_column :timelogs, :duration
+    add_column :timelogs, :duration, :string
   end
 
   def down
