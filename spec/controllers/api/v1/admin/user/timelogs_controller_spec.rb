503f8f17d23f7e56a997a6981f6d8ec7c88e5e7f
Mark <markiyan.ivancho@upwave.net>
2017-10-12 13:33:18 -0400

    factory update, specs for boards_with_cards

--- spec/controllers/api/v1/admin/user/timelogs_controller_spec.rb
+++ spec/controllers/api/v1/admin/user/timelogs_controller_spec.rb
@@ -118,4 +118,26 @@ RSpec.describe Api::V1::Admin::User::TimelogsController, type: :controller do
       expect(parsed_response).to eq(cards)
     end
   end
+
+  describe "GET #trello_boards_with_cards" do
+    it "will return error when user doesn't have trello" do
+      get :trello_boards_with_cards, format: :json, params: { user_id: user.id }
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response["errors"]).to eq('Trello connection error')
+    end
+
+    it "will return correct boards and cards" do
+      user.identities.create(
+        provider: 'trello',
+        access_token: '5c7abab8dd49ab4d9f244669820ae5120cee59d78d08f54ebf914373a3e7547a',
+        secret_token: '8238e025956453eb02120c7b962e3893',
+        uid: '59dc95fb9b71e96a24045308'
+      )
+      cards = ['Card for a test', 'Another test card']
+      board = 'My new board'
+      get :trello_boards_with_cards, format: :json, params: { user_id: user.id }
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response[board]).to eq(cards)
+    end
+  end
 end
