42265e3759fa17779cebde7000312eb1b8d17255
Roman Kan <roman95roman@gmail.com>
2017-10-24 12:26:54 +0300

    profile fixes

--- db/migrate/20171012145644_add_trello_board_to_timelogs.rb
+++ db/migrate/20171012145644_add_trello_board_to_timelogs.rb
@@ -1,5 +1,5 @@
 class AddTrelloBoardToTimelogs < ActiveRecord::Migration[5.1]
   def change
-    add_column :timelogs, :trello_board, :string, null: false
+    add_column :timelogs, :trello_board, :string
   end
 end
