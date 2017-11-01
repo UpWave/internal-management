05cf1a0b6f91098c1bbb9eae2cc377f307c3563b
Roman Kan <roman95roman@gmail.com>
2017-11-01 15:09:40 +0200

    code style fixes

--- app/models/timelog.rb
+++ app/models/timelog.rb
@@ -5,14 +5,14 @@ class Timelog < ApplicationRecord
   validates :user_id, presence: true
   validates :start_time, presence: true
   validates :duration, presence: true, numericality: { greater_than: 0 }
-  validates :task_description, presence: true, unless: ->(timelog){timelog.trello_card.present?}
-  validates :trello_card, presence: true, unless: ->(timelog){timelog.task_description.present?}
+  validates :task_description, presence: true, unless: ->(timelog) { timelog.trello_card.present? }
+  validates :trello_card, presence: true, unless: ->(timelog) { timelog.task_description.present? }
 
   before_save :set_end_time
 
-  scope :start_time, -> { order("start_time asc") }
-  scope :end_time, -> { order("end_time asc") }
-  scope :duration, -> { order("duration asc") }
+  scope :start_time, -> { order('start_time asc') }
+  scope :end_time, -> { order('end_time asc') }
+  scope :duration, -> { order('duration asc') }
   scope :ends_this_week, -> { where('end_time BETWEEN ? AND ?', Date.today, Date.today + 1.week) }
   scope :ends_this_month, -> { where('end_time BETWEEN ? AND ?', Date.today, Date.today + 1.month) }
   scope :date_range, lambda { |start_date, end_date| where ('start_time >= ? AND end_time <= ?'), start_date, end_date }
@@ -20,7 +20,7 @@ class Timelog < ApplicationRecord
 
 
   def set_end_time
-    self.end_time = self.start_time + self.duration.to_i.minutes
+    start_time + duration.to_i.minutes
   end
 
   def self.to_csv
